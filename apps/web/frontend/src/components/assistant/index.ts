8da21ded225165b4b5915f79f5f8d10dd13e1a6b
georgeprowse91 <georgeprowse91@gmail.com>
2026-02-12 14:15:06 +1100

    Expand assistant barrel exports

--- apps/web/frontend/src/components/assistant/index.ts
+++ apps/web/frontend/src/components/assistant/index.ts
@@ -3,5 +3,13 @@
  */
 
 export { AssistantPanel, default } from './AssistantPanel';
+export { AssistantHeader } from './AssistantHeader';
+export { ContextBar } from './ContextBar';
+export { MessageList } from './MessageList';
+export { MessageBubble } from './MessageBubble';
 export { ActionChipButton } from './ActionChipButton';
 export { QuickActions } from './QuickActions';
+export { ChatInput } from './ChatInput';
+export { PromptPicker } from './PromptPicker';
+export { ScopeResearchCard } from './ScopeResearchCard';
+export { ConversationalCommandCard } from './ConversationalCommandCard';
